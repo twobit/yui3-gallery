005c5e259ac66bf69d3800d9a68bce7c6dbfa6c6
Stephen Murphy <stephen@hypernaut.com>
2012-06-19 01:23:47 -0700

    added DOMNodeInserted fallback

--- src/gallery-event-inserted/js/event-inserted.js
+++ src/gallery-event-inserted/js/event-inserted.js
@@ -1,35 +1,35 @@
 /*
  * Inserted Event.
+ *
+ * Uses efficient CSS3 Animation to fire insertion events otherwise falls back
+ * to DOMNodeInserted.
  */
 var PREFIX = {
         camel: ['', 'WebKit', 'Moz', 'O', 'MS'].filter(function(prefix){
             return window[prefix + 'CSSKeyframesRule'];
         })[0]
     };
-
-/*
-Y.mix(Y.Node.DOM_EVENTS, {
-    DOMNodeInserted: true
-});*/
-
 PREFIX.lower = PREFIX.camel.toLowerCase();
-PREFIX.js = PREFIX.camel === 'WebKit' ? PREFIX.lower : PREFIX.camel;
 PREFIX.css = PREFIX.camel ? '-' + PREFIX.lower + '-' : PREFIX.camel;
 
-var ANIMATION_START_VENDORS = {
+// CSS3 Animation Support
+var Inserted = {
+    ANIMATION_START_VENDORS: {
         webkit: 'webkitAnimationStart',
         o: 'oAnimationStart'
     },
-    ANIMATION_START = ANIMATION_START_VENDORS[PREFIX.lower] || 'animationstart',
-    STYLESHEET = Y.Node.create('<style type="text/css">@' + PREFIX.css + 'keyframes INSERTED {' +
-        'from {clip: rect(1px, auto, auto, auto);} to {clip: rect(0px, auto, auto, auto);}' +
-    '}</style>');
+    ANIMATION_START: 'animationstart',
+    STYLESHEET: null,
 
-Y.Node.DOM_EVENTS[ANIMATION_START] = 1;
+    _init: function() {
+        Inserted.ANIMATION_START = Inserted.ANIMATION_START_VENDORS[PREFIX.lower] || Inserted.ANIMATION_START;
+        Y.Node.DOM_EVENTS[Inserted.ANIMATION_START] = 1;
+        Inserted.STYLESHEET = Y.Node.create('<style type="text/css">@' + PREFIX.css + 'keyframes INSERTED {' +
+            'from {clip: rect(1px, auto, auto, auto);} to {clip: rect(0px, auto, auto, auto);}' +
+        '}</style>');
+        Y.one('head').append(Inserted.STYLESHEET);
+    },
 
-Y.one('head').append(STYLESHEET);
-
-Y.Event.define('inserted', {
     processArgs: function (args, isDelegate) {
         return args.splice(2, 1)[0];
     },
@@ -38,13 +38,13 @@ Y.Event.define('inserted', {
         var method = filter ? 'delegate' : 'on',
             rule = sub._extra + '{' + PREFIX.css + 'animation-duration: 0.0001s; ' + PREFIX.css + 'animation-name: INSERTED !important;}';
 
-        sub._handle = node[method](ANIMATION_START, Y.bind(function(e) {
+        sub._handle = node[method](Inserted.ANIMATION_START, Y.bind(function(e) {
             if (e.target.get('tagName').toLowerCase() === sub._extra) {
                 notifier.fire({target: e.target});
             }
         }, this), filter);
 
-        STYLESHEET.get('sheet').insertRule(rule, 0);
+        Inserted.STYLESHEET.get('sheet').insertRule(rule, 0);
     },
 
     delegate: function() {
@@ -58,4 +58,48 @@ Y.Event.define('inserted', {
     detachDelegate: function() {
         this.detach.apply(this, arguments);
     }
-});
+};
+
+// DOMNodeInserted fallback
+var Fallback = {
+    TAGS: {},
+
+    _init: function() {
+        Y.Node.DOM_EVENTS.DOMNodeInserted = 1;
+    },
+    
+    processArgs: function (args, isDelegate) {
+        return args.splice(2, 1)[0];
+    },
+
+    on: function(node, sub, notifier, filter) {
+        var method = filter ? 'delegate' : 'on';
+
+        if (!Fallback.TAGS[sub._extra]) {
+            Y.all(sub._extra).each(function(item) {
+                notifier.fire({target: item});
+            });
+        }
+
+        sub._handle = node[method]('DOMNodeInserted', Y.bind(function(e) {
+            if (e.target.get('tagName').toLowerCase() === sub._extra) {
+                notifier.fire({target: e.target});
+            }
+        }, this), filter);
+    },
+
+    delegate: function() {
+        this.on.apply(this, arguments);
+    },
+
+    detach: function(node, sub, notifier) {
+        sub._handle.detach();
+    },
+
+    detachDelegate: function() {
+        this.detach.apply(this, arguments);
+    }
+};
+
+// Fallback if CSS3 Animation is not supported
+Y.Event.define('inserted', PREFIX.camel ? Inserted : Fallback);
