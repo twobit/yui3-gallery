f19a133be45eb40be1e153215d8329d3afbc0741
Stephen Murphy <stephen@hypernaut.com>
2012-06-28 16:31:07 -0700

    added ybind and ytarget for databinding

---
+++ src/gallery-tag-registry/js/ybind.js
@@ -0,0 +1,27 @@
+Y.Tag.register('ybind', {
+    created: function(config) {
+        var node = this.get('host');
+
+        if (config.on === 'valuechange') {
+            node = node.one('input, textarea');
+        }
+
+        node.on(config.on, function(e) {
+            Y.fire('target:' + config.name, e.target.get('value'));
+        });
+
+        if (config.init) {
+            setTimeout(function() {
+                Y.fire('target:' + config.name, config.init);
+            }, 0);
+        }
+    }
+});
+
+Y.Tag.register('ytarget', {
+    created: function(config) {
+        Y.on('target:' + config.name, function(value) {
+            this.get('host').setHTML(value);
+        }, this);
+    }
+});
